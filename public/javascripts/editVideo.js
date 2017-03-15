4d2da6ff62befdb3dcea06e50d6158b4afe83883
Piotr Filipecki <piotr.filipecki@vml.com>
2017-03-15 08:56:46 +0100

    input classes

--- public/javascripts/editVideo.js
+++ public/javascripts/editVideo.js
@@ -1,34 +1,32 @@
-
-
 var VideoCutter = {
         URL: 'http://firmseeksite3.ftp.firmseek.net/media_files/video/wysiwyg.mp4#t=',
         startTime: 0,
-        endTime: 1000,
+        endTime: 10000,
         videoDOM: document.getElementById('video'),
         init: function(){
                 this.videoDOM.addEventListener('loadedmetadata', () => {
                     this.endTime = this.videoDOM.duration;
-                    console.log(this.endTime)
+                    console.log(this.endTime);
                 }, false);
                 this.generateSrc(); 
 
                 $('#set').on('click',  () => {
-                   this.updateValues()
+                   this.updateValues();
                    
-                    console.log("1"+this.startTime)
+                    console.log(this.startTime);
                 });
 
                 $('#set2').on('click',  () => {
-                     this.updateValues()
-                    console.log(this.endTime)
+                     this.updateValues();
+                    console.log(this.endTime);
                 });
         },
         generateSrc: function() {
           return this.URL + this.startTime + "," + Math.round(this.endTime);
         },
         updateValues: function(){
-            this.endTime = $(':input:last-of-type').val();
-            this.startTime = $(':input:first-of-type').val();
+            this.endTime = $('#numberTo').val();
+            this.startTime = $('#numberFrom').val();
             this.videoDOM.src =  this.generateSrc();
         }
     }
